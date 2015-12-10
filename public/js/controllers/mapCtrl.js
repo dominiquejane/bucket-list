4658d25961bad6267a00a500126a2dfbd8d8758a
Dominique <deejay.whitegrass@gmail.com>
2015-12-10 00:07:01 -0600

    bucket populating partially functional

--- public/js/controllers/mapCtrl.js
+++ public/js/controllers/mapCtrl.js
@@ -11,16 +11,22 @@ angular.module('bucketList').controller('mapCtrl', function($scope, mapService, 
 		mapService.getBuckets().then(function(res) {
 			$scope.buckets = res.data;
 			console.log($scope.buckets);
+			for (var i = 0; i < $scope.buckets.length; i++) {
+				console.log(i);
+				mapService.populateBuckets($scope.buckets[i]);
+			}
 		});
 	};
 
 	$scope.getBuckets();
 
 	$scope.clickAddBucket = function () {
-		mapService.clickAddBucket({html: "Click edit/save to enter BucketList Item"})
+		mapService.clickAddBucket({description: "Click edit/save to enter BucketList Item"})
 	};
 
 	$scope.clickAddBucket();
 
 
 });
+
+//able to populate buckets. need to a) add a delete request when the delete button is clicked, b) correct the post/put requests on the infowindow so post only gets called once per bucket created, and the rest of the clicks result in a put/patch request when saveBtn is clicked.. This may mean taking out the post request in the editBtn function. 
--- server-assets/models/MapSchema.js
+++ server-assets/models/MapSchema.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var MapSchema = mongoose.model('MapSchema', new mongoose.Schema({
 
 	description: {type: String, index: true, required: true},
-	coordinates: {type: String},
+	coordinates: {type: Object, required: true},
 	status: {type: String, enum: ['current', 'completed'], index: true, default: 'current'},
 
 }, {timestamps: true}));
